9acb6b5abd864b98e49c65f380cde6893c3eff0b
Dario <dariokleinalves@gmail.com>
2023-09-03 16:18:33 -0300

    last version

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.src = "./assets/avatar-light.png"
+    img.src = "./assets/avatarlight.jpg"
     img.setAttribute("alt", "Imagem 2")
   } else {
-    img.src = "./assets/avatar.png"
+    img.src = "./assets/avatar.jpg"
     img.setAttribute("alt", "Imagem 1")
   }
 }
